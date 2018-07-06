4b9525a421624f9eb9521542bbe8de7e6c4cf532
Matt Hall <matthew349hall@hotmail.com>
2018-07-05 21:23:26 -0400

    article page done

--- exposed/js/shadow.js
+++ exposed/js/shadow.js
@@ -1,16 +1,17 @@
 let colorThief = new ColorThief()
-let inset = document.querySelector(".inset")
+let inset = document.querySelectorAll(".inset")
 
-
-var imgTile = new Image();
-imgTile.src = "public/img/newyork.jpg";
-imgTile.onload = function() {
-	let colors = colorThief.getColor(imgTile)
-	let r = colors[0]
-	let g = colors[1]
-	let b = colors[2]
-	let a = 0.2
-	let boxShadowString = `
+inset.forEach(img => {
+	var imgTile = new Image()
+	imgTile.src = window.getComputedStyle(img).backgroundImage.match(/\((.*?)\)/)[1].replace(/('|")/g,'')
+	console.log(imgTile.src)
+	imgTile.onload = function() {
+		let colors = colorThief.getColor(imgTile)
+		let r = colors[0]
+		let g = colors[1]
+		let b = colors[2]
+		let a = 0.2
+		let boxShadowString = `
 rgba(${r}, ${g}, ${b}, ${a}) 0px 2px 2px,
 rgba(${r}, ${g}, ${b}, ${a}) 0px 4px 4px,
 rgba(${r}, ${g}, ${b}, ${a}) 0px 8px 8px,
@@ -18,21 +19,24 @@ rgba(${r}, ${g}, ${b}, ${a}) 0px 16px 16px,
 rgba(${r}, ${g}, ${b}, ${a}) 0px 32px 32px,
 rgba(${r}, ${g}, ${b}, ${a}) 0px 64px 64px
 `
-	console.log(boxShadowString)
-	// inset.style.boxShadow = boxShadowString
-	inset.addEventListener('mouseenter', () => {
-		hoverOn(boxShadowString)
-	})	
-	inset.addEventListener('mouseout', () => {
-		hoverOff()
-	})
-}
+		console.log(boxShadowString)
 
-function hoverOn(boxShadowString) {
-	inset.style.boxShadow = boxShadowString
+		img.addEventListener('mouseenter', () => {
+			hoverOn(boxShadowString, img)
+		})	
+		img.addEventListener('mouseout', () => {
+			hoverOff(boxShadowString, img)
+		})	
+	}
+})
+
+
+function hoverOn(boxShadowString, el) {
+	console.log("hoverOn")
+	el.style.boxShadow = boxShadowString
 }
-function hoverOff() {
-	inset.style.boxShadow = "none"
+function hoverOff(boxShadowString, el) {
+	el.style.boxShadow = "none"
 }
 
 /*
